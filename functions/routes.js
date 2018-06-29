f4f5334b3e3fa82fd24a6e4aabbd1017e5aea417
Flintlozk <flintlozk@hotmail.com>
2018-06-29 18:29:19 +0700

    firebase function

---
+++ functions/routes.js
@@ -0,0 +1,30 @@
+var request = require('request');
+
+exports.getPoppular = function (req, res) {
+    let URI = 'https://api.themoviedb.org/3/movie/popular?api_key=260cea00829ef56b79e1ed27c0113e3c&language=en-US&page=' + req.body.page;
+    request.get(URI, function (error, response, body) {
+        res.send(body);
+    });
+};
+
+exports.getVideo = function (req, res) {
+    let URI = 'https://api.themoviedb.org/3/movie/' + req.body.movie_id + '/videos?api_key=260cea00829ef56b79e1ed27c0113e3c&language=en-US';
+    request.get(URI, function (error, response, body) {
+        res.send(body);
+    });
+};
+
+exports.getDetail = function (req, res) {
+    let URI = 'https://api.themoviedb.org/3/movie/' + req.body.movie_id + '?api_key=260cea00829ef56b79e1ed27c0113e3c&language=en-US';
+    request.get(URI, function (error, response, body) {
+        res.send(body);
+    });
+};
+
+exports.getReview = function (req, res) {
+    let URI = 'https://api.themoviedb.org/3/movie/' + req.body.movie_id + '/reviews?api_key=260cea00829ef56b79e1ed27c0113e3c&language=en-US&page=1';
+    request.get(URI, function (error, response, body) {
+        res.send(body);
+    });
+};
+
