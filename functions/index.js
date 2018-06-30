f3bd0e7404b49b6ed28798578ca33bd90fe0e3c7
Worawut <tonmanna@gmail.com>
2018-07-01 03:58:09 +0700

    Build FIREBASE

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,10 @@
 const functions = require("firebase-functions")
-const app = require("express")
+const express = require("express")
 var request = require('request');
 const cors = require('cors');
 const bodyParser = require("body-parser");
 
 var routepage = require('./routes');
-
-
-
-const functions = require("firebase-functions")
-const express = require("express")
 
 /* Express */
 const app = express()
@@ -20,6 +15,11 @@ app.post('/getPoppular', routepage.getPoppular); // get Poppular
 app.post('/getVideo', routepage.getVideo); // get Poppular
 app.post('/getDetail', routepage.getDetail); // get Poppular
 app.post('/getReview', routepage.getReview); // get Poppular
+
+app.get('/getPoppular', routepage.getPoppular); // get Poppular
+app.get('/getVideo', routepage.getVideo); // get Poppular
+app.get('/getDetail', routepage.getDetail); // get Poppular
+app.get('/getReview', routepage.getReview); // get Poppular
 
 /* Routing */
 app.get('*', function (req, res) {
--- functions/routes.js
+++ functions/routes.js
@@ -1,28 +1,44 @@
 var request = require('request');
 
 exports.getPoppular = function (req, res) {
-    let URI = 'https://api.themoviedb.org/3/movie/popular?api_key=260cea00829ef56b79e1ed27c0113e3c&language=en-US&page=' + req.body.page;
+    var page = req.query.page;
+    if(req.body.page!=undefined){
+        page = req.body.page
+    }
+    let URI = 'https://api.themoviedb.org/3/movie/popular?api_key=260cea00829ef56b79e1ed27c0113e3c&language=en-US&page=' + page;
     request.get(URI, function (error, response, body) {
         res.send(body);
     });
 };
 
 exports.getVideo = function (req, res) {
-    let URI = 'https://api.themoviedb.org/3/movie/' + req.body.movie_id + '/videos?api_key=260cea00829ef56b79e1ed27c0113e3c&language=en-US';
+    var movie_id = req.query.movie_id;
+    if(req.body.movie_id!=undefined){
+        movie_id = req.body.movie_id
+    }
+    let URI = 'https://api.themoviedb.org/3/movie/' + movie_id + '/videos?api_key=260cea00829ef56b79e1ed27c0113e3c&language=en-US';
     request.get(URI, function (error, response, body) {
         res.send(body);
     });
 };
 
 exports.getDetail = function (req, res) {
-    let URI = 'https://api.themoviedb.org/3/movie/' + req.body.movie_id + '?api_key=260cea00829ef56b79e1ed27c0113e3c&language=en-US';
+    var movie_id = req.query.movie_id;
+    if(req.body.movie_id!=undefined){
+        movie_id = req.body.movie_id
+    }
+    let URI = 'https://api.themoviedb.org/3/movie/' + movie_id + '?api_key=260cea00829ef56b79e1ed27c0113e3c&language=en-US';
     request.get(URI, function (error, response, body) {
         res.send(body);
     });
 };
 
 exports.getReview = function (req, res) {
-    let URI = 'https://api.themoviedb.org/3/movie/' + req.body.movie_id + '/reviews?api_key=260cea00829ef56b79e1ed27c0113e3c&language=en-US&page=1';
+    var movie_id = req.query.movie_id;
+    if(req.body.movie_id!=undefined){
+        movie_id = req.body.movie_id
+    }
+    let URI = 'https://api.themoviedb.org/3/movie/' + movie_id + '/reviews?api_key=260cea00829ef56b79e1ed27c0113e3c&language=en-US&page=1';
     request.get(URI, function (error, response, body) {
         res.send(body);
     });
