6e66ca065db5a5216a229591ccbbd6c792b7c13e
Worawut <tonmanna@gmail.com>
2018-07-01 04:05:33 +0700

    Fix Test

--- functions/routes.js
+++ functions/routes.js
@@ -1,9 +1,11 @@
 var request = require('request');
 
 exports.getPoppular = function (req, res) {
-    var page = req.query.page;
+    var page;
     if(req.body.page!=undefined){
         page = req.body.page
+    }else{
+        page = req.query.page
     }
     let URI = 'https://api.themoviedb.org/3/movie/popular?api_key=260cea00829ef56b79e1ed27c0113e3c&language=en-US&page=' + page;
     request.get(URI, function (error, response, body) {
@@ -12,9 +14,11 @@ exports.getPoppular = function (req, res) {
 };
 
 exports.getVideo = function (req, res) {
-    var movie_id = req.query.movie_id;
+    var movie_id ;
     if(req.body.movie_id!=undefined){
         movie_id = req.body.movie_id
+    }else{
+        movie_id = req.query.movie_id;
     }
     let URI = 'https://api.themoviedb.org/3/movie/' + movie_id + '/videos?api_key=260cea00829ef56b79e1ed27c0113e3c&language=en-US';
     request.get(URI, function (error, response, body) {
@@ -23,9 +27,11 @@ exports.getVideo = function (req, res) {
 };
 
 exports.getDetail = function (req, res) {
-    var movie_id = req.query.movie_id;
+    var movie_id ;
     if(req.body.movie_id!=undefined){
         movie_id = req.body.movie_id
+    }else{
+        movie_id = req.query.movie_id;
     }
     let URI = 'https://api.themoviedb.org/3/movie/' + movie_id + '?api_key=260cea00829ef56b79e1ed27c0113e3c&language=en-US';
     request.get(URI, function (error, response, body) {
@@ -34,9 +40,11 @@ exports.getDetail = function (req, res) {
 };
 
 exports.getReview = function (req, res) {
-    var movie_id = req.query.movie_id;
+    var movie_id ;
     if(req.body.movie_id!=undefined){
         movie_id = req.body.movie_id
+    }else{
+        movie_id = req.query.movie_id;
     }
     let URI = 'https://api.themoviedb.org/3/movie/' + movie_id + '/reviews?api_key=260cea00829ef56b79e1ed27c0113e3c&language=en-US&page=1';
     request.get(URI, function (error, response, body) {
